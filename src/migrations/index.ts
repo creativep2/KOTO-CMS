b657c5f87b0425524462df18e9432d1b3924da40
KiKiet <creative@projectpluto.studio>
2025-11-11 14:02:04 +0700

    remove bulk-import

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,7 +1,7 @@
 
 import * as migration_localize_blogs from './localize-blogs';
 import * as migration_localize_collections from './localize-collections';
-import * as migration_localize_pages from './localize-pages';
+//import * as migration_localize_pages from './localize-pages';
 import * as migration_add_editor_roles from './add-editor-roles';
 
 export const migrations = [
@@ -14,11 +14,6 @@ export const migrations = [
     up: migration_localize_collections.up,
     down: migration_localize_collections.down,
     name: 'localize_collections'
-  },
-  {
-    up: migration_localize_pages.up,
-    down: migration_localize_pages.down,
-    name: 'localize_pages'
   },
   {
     up: migration_add_editor_roles.up,
